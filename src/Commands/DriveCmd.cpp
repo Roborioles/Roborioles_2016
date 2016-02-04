35ed91506a0c2a21416cfc9851587ddd2abc46a1
Roborioles1 <roborioles1@gmail.com>
2016-02-04 17:47:46 -0500

    Removed call to stop

--- src/Commands/DriveCmd.cpp
+++ src/Commands/DriveCmd.cpp
@@ -40,7 +40,7 @@ bool DriveCmd::IsFinished() {
 
 // Called once after isFinished returns true
 void DriveCmd::End() {
-	Robot::chassis->Stop();
+	// Robot::chassis->Stop();
 }
 
 // Called when another command which requires one or more of the same
